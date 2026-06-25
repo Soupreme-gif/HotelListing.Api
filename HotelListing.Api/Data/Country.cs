0afe9bf744d18f594491ac7c9a9bcdf7e63de857
Soupreme-gif <dagash0401@gmail.com>
2026-06-24 23:00:22 -0500

    Added Country Data Model and amended Hotel Data Model.

---
+++ HotelListing.Api/Data/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelListing.Api.Data;
+
+public class Country
+{
+    // These three fields below are used to associate with the columns in the tables in the database.
+    public int CountryId { get; set; }
+    public String Name { get; set; }
+    public String ShortName { get; set; }
+    // This Hotels variable is called a navigation property that is used for relational operartions
+    // it is a Ilist variable because a country can have multiple hotels.
+    // The "= []" is to avoid null exceptions
+    public IList<Hotel> Hotels { get; set; } = [];
+}
--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -1,9 +1,18 @@
-﻿namespace HotelListing.Api.Data;
+﻿using System.Diagnostics.Metrics;
+
+namespace HotelListing.Api.Data;
 
 public class Hotel
 {
+    // These four fields below are used to associate with the columns in the tables in the database.
     public int Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
     public double Rating { get; set; }
+    // The CountryId variable is called a Foreign Key
+    // This country variable is called a navigation property that is used for relational operartions.
+    // it is two variables because there is only one country a hotel can be in.
+    public int CountryId { get; set; }
+    public Country? Country { get; set; }
+
 }
